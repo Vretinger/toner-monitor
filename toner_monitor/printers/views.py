87040737536463e9d444afce2b1aabe2989f6c7d
Hampus Vretinger <brozgamers@hotmail.com>
2025-01-21 14:49:31 +0000

    Added seperate toner checks

--- toner_monitor/printers/views.py
+++ toner_monitor/printers/views.py
@@ -2,9 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Printer
 from .serializers import PrinterSerializer
+from rest_framework import viewsets
+
+class PrinterViewSet(viewsets.ModelViewSet):
+    queryset = Printer.objects.all()
+    serializer_class = PrinterSerializer
 
 class PrinterList(APIView):
     def get(self, request):
         printers = Printer.objects.all()
         serializer = PrinterSerializer(printers, many=True)
         return Response(serializer.data)
+
+def index(request):
+    return render(request, 'index.html')
